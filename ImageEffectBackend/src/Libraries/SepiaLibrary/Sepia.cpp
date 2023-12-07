7a0489ae9208a6cb76650f1e80f32a5d57ace469
Sathvik <Sathvik.Rao@iiitb.ac.in>
2023-12-07 14:39:25 +0530

    Updating Sepia

--- ImageEffectBackend/src/Libraries/SepiaLibrary/Sepia.cpp
+++ ImageEffectBackend/src/Libraries/SepiaLibrary/Sepia.cpp
@@ -1,1 +1,20 @@
 #include "Sepia.h"
+using namespace std;
+#include <bits/stdc++.h>
+void applySepia(vector<vector<Pixel>>&imageVector)
+{
+    for(auto & row:imageVector)
+    {
+        for(auto &pixel:row)
+        {
+            int newRed = static_cast<int>(0.393 * pixel.r + 0.769 * pixel.g + 0.189 * pixel.b);
+            int newGreen = static_cast<int>(0.349 * pixel.r + 0.686 * pixel.g + 0.168 * pixel.b);
+            int newBlue = static_cast<int>(0.272 * pixel.r + 0.534 * pixel.g + 0.131 * pixel.b);
+
+            // Clip values to the valid range [0, 255]
+            pixel.r = min(255, max(0, newRed));
+            pixel.g = min(255, max(0, newGreen));
+            pixel.b = min(255, max(0, newBlue));
+        }
+    }
+}
--- ImageEffectBackend/src/Libraries/SepiaLibrary/Sepia.h
+++ ImageEffectBackend/src/Libraries/SepiaLibrary/Sepia.h
@@ -1,5 +1,5 @@
 #ifndef SEPIA_H
 #define SEPIA_H
 #include "../Pixel.h"
-
+void applySepia(vector<vector<Pixel>>&imageVector);
 #endif
