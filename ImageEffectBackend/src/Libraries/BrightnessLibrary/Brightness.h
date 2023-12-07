7ae9b9474cead86c1f7422f5e609a208fbc3e279
malluvkcr7 <Mallikarjun.Chakoti@iiitb.ac.in>
2023-12-07 11:55:58 +0530

    Upadted contrast and Brightness

--- ImageEffectBackend/src/Libraries/BrightnessLibrary/Brightness.h
+++ ImageEffectBackend/src/Libraries/BrightnessLibrary/Brightness.h
@@ -1,5 +1,5 @@
 #ifndef BRIGHTNESS_H
 #define BRIGHTNESS_H
 #include "../Pixel.h"
-
+vector<vector<Pixel>>& addValueToPixels(vector<vector<Pixel>>& pixels, float value);
 #endif
--- ImageEffectBackend/src/Libraries/ContrastLibrary/Contrast.cpp
+++ ImageEffectBackend/src/Libraries/ContrastLibrary/Contrast.cpp
@@ -1,1 +1,19 @@
 #include "Contrast.h"
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+
+
+vector<vector<Pixel>>& multiplyValueToPixels(vector<vector<Pixel>>& pixels, float value) {
+    for (auto& row : pixels) {
+        for (auto& pixel : row) {
+            pixel.r = min(255, static_cast<int>(pixel.r * value));
+            pixel.g = min(255, static_cast<int>(pixel.g * value));
+            pixel.b = min(255, static_cast<int>(pixel.b * value));
+        }
+    }
+    return pixels;
+}
