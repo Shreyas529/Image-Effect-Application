e386e345769a960cc7f037c506b0d3e89375d6fa
Sathvik <Sathvik.Rao@iiitb.ac.in>
2023-12-08 18:16:26 +0530

    Adding effectImplementationFolder

---
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/HueSaturationEffect.java
@@ -0,0 +1,6 @@
+package com.iiitb.imageEffectApplication.effectImplementation;
+//import com.iiitb.imageEffectApplication.baseEffects.requiredEffect;
+import com.iiitb.imageEffectApplication.libraryInterfaces.HueSaturationInterface;
+public class HueSaturationEffect {
+    
+}
