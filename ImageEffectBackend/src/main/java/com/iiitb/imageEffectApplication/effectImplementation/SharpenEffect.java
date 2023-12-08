9e550878f760fd0a5eba107f111f341d40f689d4
Sathvik S Rao <119345593+Sathvik21S21Rao@users.noreply.github.com>
2023-12-08 22:06:52 +0530

    Delete ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation directory

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementation/SharpenEffect.java
+++
@@ -1,6 +0,0 @@
-package com.iiitb.imageEffectApplication.effectImplementation;
-//import com.iiitb.imageEffectApplication.baseEffects.requiredEffect;
-import com.iiitb.imageEffectApplication.libraryInterfaces.SharpenInterface;
-public class SharpenEffect {
-    
-}
