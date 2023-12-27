f8d4ae18b8fcc6a0e13f7002adedde0a0c62a103
Sathvik <Sathvik.Rao@iiitb.ac.in>
2023-12-27 13:46:13 +0530

    More make file changes and addition of DominantInterface.cpp

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/libraryInterfaces/DominantInterface.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/libraryInterfaces/DominantInterface.java
@@ -1,5 +1,10 @@
 package com.iiitb.imageEffectApplication.libraryInterfaces;
 
 public class DominantInterface {
-    
+    static {
+        String libraryPath = "DominantLib";
+        LoadNativeLibrary.loadNativeLibrary(libraryPath);
+    }
+    public static native Pixel[][] applyDominant(Pixel[][] image);
 }
+
